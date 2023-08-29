00bbd26943778f2acc4d25a814dd2f8bbb4ebd93
EugeniuPeruzzi <jaka1997@live.it>
2023-08-29 18:05:48 +0200

    exercises done

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -3,19 +3,24 @@ import axios from 'axios';
 
 
 export default {
+    name: 'AppMain',
     data() {
         return {
-            baseUrl: 'http://127.0.0.1:8000/',
-            loading: true,
+            baseUrl: 'http://localhost:8000',
+            posts: []
         }
     },
+
+    created() {
+        this.getData();
+    },
+
     methods: {
         getData() {
             // chiamata axios che usa la base url con aggiunta della parte final della chiamata api
-            axios.get(`${baseUrl}/api/posts`).then((response) => {
+            axios.get(`${this.baseUrl}/api/posts`).then((response) => {
                 if (response.data.success) {
-                    this.post = response.data.results;
-                    this.loading = false;
+                    this.posts = response.data.results;
                 }
             });
         }
@@ -25,7 +30,43 @@ export default {
 </script>
 
 <template>
-    <h1>ciaoo come va?</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="text-center">BOOLFOLIO</h1>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-4" v-for="post in  posts " :key="post.id">
+                <div class="card my-3 min-height-200px">
+                    <div class="card-image-top">
+                        <img :src="`${baseUrl}/storage/${post.image}`" class="img-fluid">
+                    </div>
+                    <div class="card-body">
+                        <div class="card-title">
+                            <h2>{{ post.title }}</h2>
+                        </div>
+                        <p>
+                            <span v-if="post.category"><strong>{{ post.category.name }}</strong></span>
+                            <span v-else><strong>Tipo non assegnato</strong></span>
+                        </p>
+                        <p v-if="post.technologies">
+                            <span class="badge text-bg-primary me-2" v-for=" technology  in  post.technologies "
+                                :key="technology.id">
+                                {{ technology.name }}
+                            </span>
+                        </p>
+                        <p>{{ post.content }}</p>
+                    </div>
+                    <div class="card-footer">
+                        <a href="#" class="btn btn-sm btn-primary"> Leggi il progetto</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 
