91050fdf2ec26dc12c0c0731e14f2c38154540a5
EugeniuPeruzzi <jaka1997@live.it>
2023-08-29 17:19:04 +0200

    axios done

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,9 +1,31 @@
 <script>
+import axios from 'axios';
 
+
+export default {
+    data() {
+        return {
+            baseUrl: 'http://127.0.0.1:8000/',
+            loading: true,
+        }
+    },
+    methods: {
+        getData() {
+            // chiamata axios che usa la base url con aggiunta della parte final della chiamata api
+            axios.get(`${baseUrl}/api/posts`).then((response) => {
+                if (response.data.success) {
+                    this.post = response.data.results;
+                    this.loading = false;
+                }
+            });
+        }
+    }
+
+}
 </script>
 
 <template>
-    <h1>CIaooo</h1>
+    <h1>ciaoo come va?</h1>
 </template>
 
 
